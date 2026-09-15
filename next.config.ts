26e9d24914585d23188c18128ebc0c9581bd7c75
juan <juanpablo.moyano.7@gmail.com>
2026-09-14 21:51:51 -0300

    Se ajusto el limite para la carga de fotos a 5mb

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb', // Ajusta este valor ('2mb', '10mb', etc.) según tus necesidades
+    },
+  },
 };
 
 export default nextConfig;
