fd1d053e89571cf9208124657f0ba00492c18d98
juan <juanpablo.moyano.7@gmail.com>
2026-09-17 15:06:41 -0300

    Se corrigio nombre de archivo middleware por proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const isAccessingAdmin = request.nextUrl.pathname.startsWith("/admin");
   const isAccessingLogin = request.nextUrl.pathname === "/login";
   const hasSupabaseConfig = Boolean(
